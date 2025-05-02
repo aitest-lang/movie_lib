d66b0659ae5bc8c39e99535e70bc828a6cba5583
aitest-lang <aitestall@outlook.com>
2025-05-02 15:31:18 +0530

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,48 @@
+// Local dataset (fallback)
+const localMovies = [
+    {
+        id: 1,
+        title: "Baahubali: The Beginning",
+        year: 2015,
+        poster: "https://m.media-amazon.com/images/M/MV5BYWVlMjVhZWYtNWViNC00ODFkLTk1MmItYjU1MDY5ZDdhMTU3XkEyXkFqcGdeQXVyODIwMDI1NjM@._V1_SX300.jpg",
+        language: "Telugu",
+        type: "Movie",
+        rating: 8.0
+    },
+    {
+        id: 2,
+        title: "RRR",
+        year: 2022,
+        poster: "https://m.media-amazon.com/images/M/MV5BODUwNDNjYzctODUxNy00ZTA2LWIyYTEtMDc5Y2E5ZjBmNTMzXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
+        language: "Telugu",
+        type: "Movie",
+        rating: 7.8
+    },
+    {
+        id: 3,
+        title: "Dangal",
+        year: 2016,
+        poster: "https://m.media-amazon.com/images/M/MV5BMTQ4MzQzMzM2Nl5BMl5BanBnXkFtZTgwMTQ1NzU3MDI@._V1_SX300.jpg",
+        language: "Hindi",
+        type: "Movie",
+        rating: 8.3
+    },
+    {
+        id: 4,
+        title: "Breaking Bad",
+        year: 2008,
+        poster: "https://m.media-amazon.com/images/M/MV5BYTU3NWI5OGMtZmZhNy00MjVmLTk1YzAtZjA3ZDA3NzcyNDUxXkEyXkFqcGdeQXVyODY5Njk4Njc@._V1_SX300.jpg",
+        language: "English",
+        type: "TV Series",
+        rating: 9.5
+    },
+    {
+        id: 5,
+        title: "Attack on Titan",
+        year: 2013,
+        poster: "https://m.media-amazon.com/images/M/MV5BMTY5ODk1NzUyMl5BMl5BanBnXkFtZTgwMjUyNzEyMTE@._V1_SX300.jpg",
+        language: "Japanese",
+        type: "Anime",
+        rating: 9.0
+    }
+];
